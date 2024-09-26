c25a2d5524b0d3b6004bebdc7a11fe554f92d728
Lidan4315 <faiqlidan15@gmail.com>
2024-09-26 13:40:20 +0700

    feat: add Tugas3

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post 
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Faiq Lid',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur itaque incidunt blanditiis! Dolores rem quibusdam fugiat amet ducimus, nemo iste aperiam, voluptates laborum, sequi voluptas obcaecati laboriosam ullam sint harum.'
-            ],
-    
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Faiq Lidan',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo possimus, officiis odio dicta quam ea atque repudiandae vero, sapiente quo ducimus quasi rem veritatis a quas accusantium consequatur molestias nulla.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if(! $post){
-            abort(404);
-        }
-
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
